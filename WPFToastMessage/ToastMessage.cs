60ca9460d27d185b87f1bca938c3f5a0724f53ac
Alexander Håkansson <al3xand3r.h@gmail.com>
2015-01-22 16:18:04 +0100

    Initial commit

---
+++ WPFToastMessage/ToastMessage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Se.Creotec.WPFToastMessage
+{
+    class ToastMessage
+    {
+        /// <summary>
+        /// Display a toast message for a defined amount of time
+        /// </summary>
+        /// <param name="message">The message to be shown</param>
+        /// <param name="delay">Time in milliseconds that the toast will be shown</param>
+        public static void Show(String message, int delay)
+        {
+            // Dummy action
+            new ToastWindow().Show();
+        }
+    }
+}
---
+++ WPFToastMessage/ToastWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Se.Creotec.WPFToastMessage
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class ToastWindow : Window
+    {
+        public ToastWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ToastMessage.Show("I work!", 1000);
+        }
+    }
+}
